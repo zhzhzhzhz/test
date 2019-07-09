7df2fa432c4b828fd39159a053a76059df08ef85
zh <1453278716@qq.com>
2019-07-09 16:16:13 +0800

    添加了一个实体类

---
+++ src/main/java/aaa/springboot02/entity/Dept.java
@@ -0,0 +1,22 @@
+package aaa.springboot02.entity;
+
+public class Dept {
+    private Integer deptno;
+    private String dname;
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+}
